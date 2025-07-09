b2dc9137b18e6ce4c4b4509e7eca925ee3182695
Pavithra S <pavithra.s2023ece@sece.ac.in>
2025-07-09 22:29:56 +0530

    Day 6 Springboot

--- Springboot-Internship/src/main/java/com/example/Springboot_Internship/config/SpringConfiguration.java
+++ Springboot-Internship/src/main/java/com/example/Springboot_Internship/config/SpringConfiguration.java
@@ -1,9 +1,12 @@
 package com.example.Springboot_Internship.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SpringConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -23,7 +27,14 @@ public class SpringConfiguration {
     SecurityFilterChain securityfilterChain(HttpSecurity http) throws Exception{
         http.
                 csrf((csrf)->csrf.disable())
-                .authorizeHttpRequests(auth-> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth-> {
+
+                    auth.requestMatchers(HttpMethod.POST,"/employee").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.PUT,"/employee").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.DELETE,"/employee").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.GET,"/").hasRole("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
                 .httpBasic(Customizer.withDefaults());
         return http.build();
 
