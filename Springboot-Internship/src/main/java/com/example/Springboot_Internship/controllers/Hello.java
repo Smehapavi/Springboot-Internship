c993103a9f88d5e445ede0bb987f84032b1ac65d
Pavithra S <pavithra.s2023ece@sece.ac.in>
2025-07-14 15:48:22 +0530

    Backend

---
+++ Springboot-Internship/src/main/java/com/example/Springboot_Internship/controllers/Hello.java
@@ -0,0 +1,7 @@
+package com.example.Springboot_Internship.controllers;
+
+public class Hello {
+    public String helloTest(){
+        return "Hello Test";
+    }
+}
--- Springboot-Internship/src/test/java/com/example/Springboot_Internship/SpringbootInternshipApplicationTests.java
+++ Springboot-Internship/src/test/java/com/example/Springboot_Internship/SpringbootInternshipApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springboot_InternshipApplicationTests {
+class SpringbootInternshipApplicationTests {
 
 	@Test
 	void contextLoads() {
