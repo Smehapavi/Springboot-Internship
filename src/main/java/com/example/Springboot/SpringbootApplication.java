0a245804c3a691b806d076e9063458a8a0c1adcc
Pavithra S <pavithra.s2023ece@sece.ac.in>
2025-07-16 11:18:45 +0530

    Railway

--- src/main/java/com/example/Springboot/SpringbootApplication.java
+++ src/main/java/com/example/Springboot/SpringbootApplication.java
@@ -1,18 +1,15 @@
 package com.example.Springboot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.Springboot.repository")
+@EntityScan(basePackages = "com.example.Springboot.models")
 public class SpringbootApplication {
-
-//	@Value("${app.jwt-Secret}")
-//	private static String jwtSecret;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
-		//System.out.println("JWT KEY" + jwtSecret);
 	}
-
 }
